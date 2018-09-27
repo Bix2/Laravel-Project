df6bf171716a2a9c51c92f679d2802873af5566e
Bix2 <bibi.treffers@outlook.com>
2018-09-27 11:16:20 +0200

    routes and views for login, user and dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function() {
+    return view('login');
+});
+
+Route::get('/user', function() {
+    return view('user');
+});
+
+Route::get('/dashboard', function() {
+    return view('dashboard');
+});
+/*Route::get('/project/{slug}', function($slug) {
+    return response()->json(\App\Project::where('slug', '=', $slug)->first());
+});*/
