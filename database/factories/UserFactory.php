1efba1e5563c95a731187a661d67d02781f2b4d1
RafaelDesignBE <thegoldenboxmail@gmail.com>
2018-09-28 10:50:11 +0200

    Add users seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,10 +15,13 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'token' => $faker->sha1,
+        'length' => $faker->randomFloat($nbMaxDecimals = 2, $min = 160, $max = 200),
+        'weight' => $faker->randomFloat($nbMaxDecimals = 2, $min = 50, $max = 100)
     ];
 });
