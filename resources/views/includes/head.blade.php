97c3b102eeb7c3593e189ff3d3304069c1165c30
robertvaida@hotmail.com <robertvaida@hotmail.com>
2018-10-03 14:58:12 +0200

    bootstrap added

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -4,7 +4,7 @@
 <meta name="author" content="Scotch">
 <title>Code Breaker</title>
 <!-- load bootstrap from a cdn -->
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/3.0.3/css/bootstrap-combined.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <!-- Fonts -->
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 <!-- Styles -->
