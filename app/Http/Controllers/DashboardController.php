dd33ede88100fafad7bcd44620e823adfb495bf5
RafaelDesignBE <thegoldenboxmail@gmail.com>
2018-10-04 12:18:53 +0200

    Redirect dashboard to homepage

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,7 +7,11 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index() {
-        $user = session()->get('user');
-        return view('dashboard')->with(['user' => $user]);
+        if (session()->has('user')) {
+            $user = session()->get('user');
+            return view('dashboard')->with(['user' => $user]);
+        } else {
+            return redirect()->intended('/');
+        }
     }
 }
