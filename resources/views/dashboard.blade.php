0a0378a2106f7816f9369ee733d3c727173940a3
Bix2 <bibi.treffers@outlook.com>
2018-10-12 15:31:26 +0200

    radio

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,7 @@
 			<div class="top__content col-8">
 				<h2>Dashboard</h2>
 				<p>Welcome to CodeBreak dashboard</p>
+                <p class="suggestion">Maybe you can go for a walk?</p>
 			</div>
 			<div class="col-4 top__content profile__picture">
                 <div class="profile__picture--wrapper">
@@ -18,7 +19,7 @@
 		</div>
     </div>
 
-   @if ( $totalsteps >= $stepsgoal )
+    @if ( $totalsteps >= $stepsgoal )
     <div class="col-8">
         <div class="card">
             <div class="card-header">How are you feeling?</div>
@@ -27,22 +28,22 @@
                     {{ csrf_field() }}
                     <h3>How are you feeling?</h3>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" id="bad" name="mood" value="bad">
+                        <input class="form-check-input" type="radio" id="bad" name="mood" value="bad">
                         <label class="form-check-label" for="mood">Bad &#x1F614;</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" id="neutral" name="mood" value="neutral">
+                        <input class="form-check-input" type="radio" id="neutral" name="mood" value="neutral">
                         <label class="form-check-label" for="mood">Neutral &#x1F610;</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" id="good" name="mood" value="good">
+                        <input class="form-check-input" type="radio" id="good" name="mood" value="good">
                         <label class="form-check-label" for="mood">Good &#x1F917;</label>
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
         </div>
-    </div>  
+    </div>
     @endif
 
 	<div class="col-12 main__content">
@@ -58,31 +59,7 @@
                         {{ $api->showWater() }};
                     @elseif ($habit->type == "exercise")
                         {{ $api->showSteps() }}
-                        <div class="goal_progress_bar">
-                            <div class="goal_progress_bar_progress" style="width: {{$totalsteps/$stepsgoal*100}}%; background-color: @if ( ($totalsteps/$stepsgoal*100) < 25 )
-                            #E51C23
-                            @elseif ( ($totalsteps/$stepsgoal*100) < 50 )
-                            #FF9800
-                            @elseif ( ($totalsteps/$stepsgoal*100) < 75 )
-                            #FFEB3B
-                            @else
-                            #259B24
-                            @endif
-                            ">
-                             </div>
-                             <div class="goal_progress__progress_text"> {{$totalsteps}} out of {{$stepsgoal}}</div>
-                            
-                        </div>
-                        @if ( ($totalsteps/$stepsgoal*100) < 25 )
-                            <p class="goal_progress_text">You could go for a walk...</p>
-                            @elseif ( ($totalsteps/$stepsgoal*100) < 75 )
-                            <p class="goal_progress_text">Come on, you can reach your goal</p>
-                            @elseif ( ($totalsteps/$stepsgoal*100) < 100 )
-                            <p class="goal_progress_text">Almost there...</p>
-                            @else
-                            <p class="goal_progress_text">🏁🏁🏁Woohoo! You've reached your goal!</p>
-                        @endif
-                        <!-- <p class="goal_progress_text"> {{$totalsteps}} out of {{$stepsgoal}}</p> -->
+                        <p> {{$totalsteps}} out of {{$stepsgoal}}</p>
                     @elseif ($habit->type == "breathing")
                         <a href="">Start guided breathing session</a>
                     @endif
