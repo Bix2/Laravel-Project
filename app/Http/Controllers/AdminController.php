5f5f85285fc9af110eacff1db95238f8988f0c63
robertvaida@hotmail.com <robertvaida@hotmail.com>
2018-10-18 21:55:14 +0200

    admin role middleware

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,18 +12,14 @@ class AdminController extends Controller
             // save user data to variable $me
             $me = Auth::user();
             $data['user'] = $me;
-            if ($me->admin == 1) {
-                $users = \DB::table('user_moods')
-                ->join('users', 'user_moods.user_id', '=', 'users.id')
-                ->join('habits', 'user_moods.habit_id', '=', 'habits.id')
-                ->select('user_moods.mood', 'users.name', 'users.admin', 'habits.type')
-                ->orderBy('users.name')
-                ->get();
-                $data['users'] = $users;
-                return view('admin', $data);
-            } else {
-                return view('admin', $data);
-            }
+            $users = \DB::table('user_moods')
+            ->join('users', 'user_moods.user_id', '=', 'users.id')
+            ->join('habits', 'user_moods.habit_id', '=', 'habits.id')
+            ->select('user_moods.mood', 'users.name', 'users.admin', 'habits.type')
+            ->orderBy('users.name')
+            ->get();
+            $data['users'] = $users;
+            return view('admin', $data);
         }
     }
 }
