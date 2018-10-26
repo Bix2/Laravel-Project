0bdb7528edfc76eff1eb5644a2e2ade52b81698e
RafaelDesignBE <thegoldenboxmail@gmail.com>
2018-10-26 15:23:17 +0200

    Fix tracking habits bug

--- app/Http/Controllers/HabitController.php
+++ app/Http/Controllers/HabitController.php
@@ -39,16 +39,7 @@ class HabitController extends Controller
     public function track($habit) {
         $data = Habit::trackHabit($habit);
         // TESTING: add data from steps activity (it should add the data from the actual habit instead)
-        if($habitId == 1) {
-            $data['sleepweek'] = Habit::getTrackedSleepLogsData();
-        } else if($habitId == 2) {
-            $data['waterweek'] = Habit::getTrackedWaterLogsData();
-        } else if($habitId == 3) {
-            $data['breathingweek'] = Habit::getTrackedBreathingData();
-        } else if($habitId == 4) {
-            $data['stepsweek'] = Habit::getTrackedActivityStepsData();
-        }
-        return view('habit', $data);
+        return redirect('/dashboard/'.$habit);
     }
 
     // public function showbreath($habit) {
