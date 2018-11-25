35b8ff38acb20504f47e0f848b0dbb270b08d260
robertvaida@hotmail.com <robertvaida@hotmail.com>
2018-11-25 14:11:30 +0100

    add responsiveness to week sleep chart

--- public/js/app.js
+++ public/js/app.js
@@ -47552,134 +47552,148 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
-    name: 'BarExample',
-    components: {
-        weeksleepchart: __WEBPACK_IMPORTED_MODULE_0_vue_apexcharts___default.a
-    },
-    data: function data() {
-        return {
-            chartOptions: {
-                chart: {
-                    height: 350,
-                    type: 'bar',
-                    stacked: true
-                },
-                plotOptions: {
-                    bar: {
-                        horizontal: true
-                    }
-                },
-                xaxis: {
-                    categories: []
-                },
-                yaxis: {
-                    title: {
-                        text: undefined
-                    }
-                },
-                dataLabels: {
-                    enabled: false
-                },
-                title: {
-                    align: 'center',
-                    text: 'Weekly Plan'
-                },
-                tooltip: {
-                    colors: ['#E14DA5', '#F9DA69', '#AB64E1', '#58CFD7'],
-                    shared: false,
-                    y: {
-                        formatter: function formatter(val) {
-                            return val + " min";
-                        }
-                    }
-                },
-                fill: {
-                    colors: ['#E14DA5', '#F9DA69', '#AB64E1', '#58CFD7']
-                },
-                states: {
-                    hover: {
-                        filter: 'none'
-                    }
-                },
-                legend: {
-                    position: 'right'
-                }
+  name: 'BarExample',
+  components: {
+    weeksleepchart: __WEBPACK_IMPORTED_MODULE_0_vue_apexcharts___default.a
+  },
+  data: function data() {
+    return {
+      series: [{
+        name: 'Light',
+        data: [0, 0, 0, 0, 0, 0, 0]
+      }, {
+        name: 'REM',
+        data: [0, 0, 0, 0, 0, 0, 0]
+      }, {
+        name: 'Deep',
+        data: [0, 0, 0, 0, 0, 0, 0]
+      }, {
+        name: 'Awake',
+        data: [0, 0, 0, 0, 0, 0, 0]
+      }],
+      chartOptions: {
+        responsive: [{
+          breakpoint: 1000,
+          options: {
+            plotOptions: {
+              bar: {
+                horizontal: false
+              }
             },
-            series: [{
-                name: 'Light',
-                data: [0, 0, 0, 0, 0, 0, 0]
-            }, {
-                name: 'REM',
-                data: [0, 0, 0, 0, 0, 0, 0]
-            }, {
-                name: 'Deep',
-                data: [0, 0, 0, 0, 0, 0, 0]
-            }, {
-                name: 'Awake',
-                data: [0, 0, 0, 0, 0, 0, 0]
-            }]
-        };
-    },
-    created: function created() {
-        var self = this;
-        var last7Days = [];
-        var daysNotActive = [];
-        var daysActive = [];
-        var seriesArray = [[], [], [], []];
-        for (var i = 0; i < 7; i++) {
-            var days = i; // Days you want to subtract
-            var date = new Date();
-            var last = new Date(date.getTime() - days * 24 * 60 * 60 * 1000);
-            var day = ("0" + last.getDate()).slice(-2);
-            var month = last.getMonth() + 1;
-            var year = last.getFullYear();
-            var createdDate = year + "-" + month + "-" + day;
-            last7Days.push(createdDate);
+            legend: {
+              position: 'bottom'
+            }
+          }
+        }],
+        chart: {
+          // height: 350,
+          // type: 'bar',
+          stacked: true,
+          id: 'chart_sleep'
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true
+          }
+        },
+        xaxis: {
+          categories: []
+        },
+        yaxis: {
+          title: {
+            text: undefined
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        title: {
+          align: 'center',
+          text: 'Weekly Plan'
+        },
+        tooltip: {
+          colors: ['#E14DA5', '#F9DA69', '#AB64E1', '#58CFD7'],
+          shared: false,
+          y: {
+            formatter: function formatter(val) {
+              return val + " min";
+            }
+          }
+        },
+        fill: {
+          colors: ['#E14DA5', '#F9DA69', '#AB64E1', '#58CFD7']
+        },
+        states: {
+          hover: {
+            filter: 'none'
+          }
+        },
+        legend: {
+          position: 'right'
         }
-        self.chartOptions.xaxis.categories = [last7Days[0], last7Days[1], last7Days[2], last7Days[3], last7Days[4], last7Days[5], last7Days[6]];
-        axios.get('/api/getweeksleep').then(function (response) {
-            for (var n = 0; n < last7Days.length; n++) {
-                for (var n2 = 0; n2 < response.data.length; n2++) {
-                    if (last7Days[n] == response.data[n2].date_of_sleep) {
-                        var data = {
-                            date: last7Days[n],
-                            lightSleep: response.data[n2].light_minutes,
-                            remSleep: response.data[n2].rem_minutes,
-                            wakeSleep: response.data[n2].wake_minutes,
-                            deepSleep: response.data[n2].deep_minutes
-                        };
-                        daysActive.push(data);
-                    }
-                }
-            }
-            var counter = 0;
-            for (var _i = 0; _i < last7Days.length; _i++) {
-                if (daysActive[counter] != undefined) {
-                    if (last7Days[_i] == daysActive[counter].date) {
-                        seriesArray[0].push(daysActive[counter].lightSleep);
-                        seriesArray[1].push(daysActive[counter].remSleep);
-                        seriesArray[2].push(daysActive[counter].deepSleep);
-                        seriesArray[3].push(daysActive[counter].wakeSleep);
-                        counter++;
-                    } else {
-                        seriesArray[0].push(0);
-                        seriesArray[1].push(0);
-                        seriesArray[2].push(0);
-                        seriesArray[3].push(0);
-                    }
-                } else {
-                    seriesArray[0].push(0);
-                    seriesArray[1].push(0);
-                    seriesArray[2].push(0);
-                    seriesArray[3].push(0);
-                }
-            }
-            self.series[0].data = seriesArray[0];
-            self.series[1].data = seriesArray[1];
-            self.series[2].data = seriesArray[2];
-            self.series[3].data = seriesArray[3];
-        });
+      }
+    };
+  },
+  created: function created() {
+    var self = this;
+    var last7Days = [];
+    var daysNotActive = [];
+    var daysActive = [];
+    var seriesArray = [[], [], [], []];
+    for (var i = 0; i < 7; i++) {
+      var days = i; // Days you want to subtract
+      var date = new Date();
+      var last = new Date(date.getTime() - days * 24 * 60 * 60 * 1000);
+      var day = ("0" + last.getDate()).slice(-2);
+      var month = last.getMonth() + 1;
+      var year = last.getFullYear();
+      var createdDate = year + "-" + month + "-" + day;
+      last7Days.push(createdDate);
     }
+    self.chartOptions.xaxis.categories = [last7Days[0], last7Days[1], last7Days[2], last7Days[3], last7Days[4], last7Days[5], last7Days[6]];
+    axios.get('/api/getweeksleep').then(function (response) {
+      for (var n = 0; n < last7Days.length; n++) {
+        for (var n2 = 0; n2 < response.data.length; n2++) {
+          if (last7Days[n] == response.data[n2].date_of_sleep) {
+            var data = {
+              date: last7Days[n],
+              lightSleep: response.data[n2].light_minutes,
+              remSleep: response.data[n2].rem_minutes,
+              wakeSleep: response.data[n2].wake_minutes,
+              deepSleep: response.data[n2].deep_minutes
+            };
+            daysActive.push(data);
+          }
+        }
+      }
+      var counter = 0;
+      for (var _i = 0; _i < last7Days.length; _i++) {
+        if (daysActive[counter] != undefined) {
+          if (last7Days[_i] == daysActive[counter].date) {
+            seriesArray[0].push(daysActive[counter].lightSleep);
+            seriesArray[1].push(daysActive[counter].remSleep);
+            seriesArray[2].push(daysActive[counter].deepSleep);
+            seriesArray[3].push(daysActive[counter].wakeSleep);
+            counter++;
+          } else {
+            seriesArray[0].push(0);
+            seriesArray[1].push(0);
+            seriesArray[2].push(0);
+            seriesArray[3].push(0);
+          }
+        } else {
+          seriesArray[0].push(0);
+          seriesArray[1].push(0);
+          seriesArray[2].push(0);
+          seriesArray[3].push(0);
+        }
+      }
+      self.series[0].data = seriesArray[0];
+      self.series[1].data = seriesArray[1];
+      self.series[2].data = seriesArray[2];
+      self.series[3].data = seriesArray[3];
+    });
+  }
 });
 
 /***/ }),
@@ -47698,11 +47712,10 @@ var render = function() {
   var _c = _vm._self._c || _h
   return _c(
     "div",
-    { staticClass: "chart chart__sleep" },
+    { staticClass: "chart chart__sleep", attrs: { id: "chart_sleep" } },
     [
       _c("weeksleepchart", {
         attrs: {
-          width: "100%",
           height: "350",
           type: "bar",
           options: _vm.chartOptions,
