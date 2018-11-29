f2a44192c5271a868d26bf81cc3d74a4b0a41203
Bix2 <bibi.treffers@outlook.com>
2018-11-29 06:32:49 +0100

    added register/signup button on welcomepage

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,7 @@
 <header class="main__header">
         <div class="logo__container">
             <a class="logo__container__link" href="#">
-                <img class="main__logo" src="/images/logo.svg" alt="Code Break logo">
+                <img class="main__logo" src="/images/full-logo-white.svg" alt="Code Break logo">
             </a>
         </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,61 +5,20 @@
 </head>
 <body class="welcome-page">
 
-            <!-- <div class="welcome_wrapper">
-            <header class="main__header">
-                <div class="row">
-                    <div class="logo__cointainer col-6">
-                        <a class="logo__container__link" href="#">
-                            <img class="main__logo" src="/images/logo.svg" alt="Code Break logo">
-                        </a>
-                    </div>
-                </div>
-            </header>
-            <div class="welcome__content">
-                <div class="welcome_image"></div>
-                <div class="welcome_slant"></div>
-                <div class="welcome_login">
-                    <h2 class="header_tagline">Mindfulness for developers, boost your concentration through habit tracking.</h2>
-                    <div class="login-button">
-                        <a class="btn btn-primary" href="{{ url("login/fitbit") }}">Login with Fitbit</a>
-                        <div class="login__info">All you need is a Fitbit account</div> 
-                    </div>
-                    
-                    <div class="dailystats">
-                        <h3>What users have done today</h3>
-                        <h4 class="dailytotal"><span class="dailyamount" data-count="{{$total_sleep}}">0</span> minutes slept today</h4>
-                        <ul>
-                            <li class="dailytotal"><span class="dailyamount" data-count="{{$rem_minutes}}">0</span> minutes rem sleep</li>
-                            <li class="dailytotal"><span class="dailyamount" data-count="{{$deep_minutes}}">0</span> minutes deep sleep</li>
-                            <li class="dailytotal"><span class="dailyamount" data-count="{{$light_minutes}}">0</span> minutes light sleep</li>
-                        </ul>
-                        <h4 class="dailytotal"><span class="dailyamount" data-count="{{$dailywater}}">0</span> liters of water drunk today</h4>
-                        <h4 class="dailytotal"><span class="dailyamount" data-count="{{$dailybreathing}}">0</span> breathing sessions done today</h4>
-                        <h4 class="dailytotal"><span class="dailyamount" data-count="{{$dailysteps}}">0</span> steps taken today</h4>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-        
-        <!-- 
-        bg-heart.svg
-        bg-sleep.svg
-        bg-stats.svg
-        home-bg.svg
-        home-sec-img.svg
-        -->
-
-        <!-- Container -->
         <div class="container-fluid">
             <div class="row">
                 <div class="container--bottom col-md-6">
                     <div class="welcome--logo">
-                        <img src="/images/logo.svg" alt="codebreak logo">
+                        <img src="/images/full-logo.svg" alt="codebreak logo">
                     </div>
                     <div class="welcome--text">
-                        <h1>Mindfulness for developers</h1>
+                        <h1 class="mb-5">Mindfulness for developers</h1>
                         <h5>Boost your concentration through habit tracking. All you need is a FitBit account</h5>
-                        <a class="mt-5 btn-lg btn btn-success" href="{{ url('login/fitbit') }}">Login with Fitbit</a>
+                        <a class="mt-3 mb-5 btn-lg btn btn-success" href="{{ url('login/fitbit') }}">Login with Fitbit</a>
+                    </div>
+                    <div class="welcome--text">
+                        <p>Dont have an account yet? Sign up now for a free account!</p>
+                        <a class="btn-lg btn btn-register" href="https://www.fitbit.com/be/signup">Register</a>
                     </div>
                     <div class="bg-sec"></div>
                 </div>
